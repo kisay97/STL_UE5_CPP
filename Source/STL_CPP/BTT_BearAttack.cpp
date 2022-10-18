10850cdab2f21a37ef7f8da5389d9c3923e8d1b0
kisay97 <kisay97@naver.com>
2022-10-18 13:44:10 +0900

    BTT_BearAttack 구현부 작성

--- Source/STL_CPP/BTT_BearAttack.cpp
+++ Source/STL_CPP/BTT_BearAttack.cpp
@@ -3,6 +3,7 @@
 
 #include "BTT_BearAttack.h"
 #include "BearAIController.h"
+#include "Bear.h"
 
 UBTT_BearAttack::UBTT_BearAttack()
 {
@@ -12,14 +13,27 @@ UBTT_BearAttack::UBTT_BearAttack()
 
 EBTNodeResult::Type UBTT_BearAttack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
+	EBTNodeResult::Type r = Super::ExecuteTask(OwnerComp, NodeMemory);
+
 	// TODO
 	// 1. 공격 실행 Bear->Attack();
 	// 2. finish 호출 불가
 	// 3. return::InProgress
-	return EBTNodeResult::Type();
+	auto ControlledBear = Cast<ABear>(OwnerComp.GetAIOwner()->GetPawn());
+	if (ControlledBear)
+	{
+		ControlledBear->Attack();
+		return EBTNodeResult::InProgress;
+	}
+	return EBTNodeResult::Failed;
 }
 
 void UBTT_BearAttack::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
 	Super::TickTask(OwnerComp, NodeMemory, DeltaSeconds);
+	auto ControlledBear = Cast<ABear>(OwnerComp.GetAIOwner()->GetPawn());
+	if (!ControlledBear->IsAttacking)
+	{
+		FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded); //latent : 숨어있는. 마지막 태스크를 피니시 시킨다.
+	}
 }
